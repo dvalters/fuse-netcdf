55682b82d6e9cf404439cfa1c076952eee3e254d
Declan Valters <declan.valters@gmail.com>
2018-06-27 16:57:38 +0100

    Fix to 'bad address' when mounting netCDF

--- fuse-netcdf/fusenetcdf.py
+++ fuse-netcdf/fusenetcdf.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
 """
@@ -28,7 +29,12 @@ class NetCDFFUSE(Operations):
 
   class PotentialNetCDFFile:
     def __init__(self, path):
-      self.data_attrs = {}
+      self.dsattrs = { "user.ndim" : (lambda x : x.value.ndim), 
+        "user.shape" : (lambda x : x.value.shape), 
+        "user.dtype" : (lambda x : x.value.dtype), 
+        "user.size" : (lambda x : x.value.size), 
+        "user.itemsize" : (lambda x : x.value.itemsize), 
+        "user.dtype.itemsize" : (lambda x : x.value.dtype.itemsize),}
       self.fullpath = path
       self.internalpath = "/"
       self.dataset_handle = None
@@ -42,6 +48,7 @@ class NetCDFFUSE(Operations):
           test = "/".join(components[:i])
           if self.testNetCDF(test):
             self.internalpath = "/".join(components[i-len(components):])
+            print self.internalpath
             break
 
         # handle this case better - I think it can be done better with 
@@ -53,9 +60,10 @@ class NetCDFFUSE(Operations):
           # Also test for netCDF version here?
           self.dataset_handle = ncpy.Dataset(path, "r")
           self.dataset_file = path
+          print path + " is netCDF"
           return True
-        except:
-          pass
+        except exc as e:
+          print e
         return False
 
     def __del__(self):
@@ -67,55 +75,82 @@ class NetCDFFUSE(Operations):
           pass
 
     def makeIntoDir(self, statdict):
-      raise NotImplementedError()
+      """Update the statdict if the item in the VFS should be
+      presented as a directory
+      """
+      print "Making a statdict!"
+      statdict["st_mode"] = statdict["st_mode"] ^ 0100000 | 0040000
+      for i in [ [ 0400 , 0100 ] , [ 040 , 010 ] , [ 04, 01 ] ]:
+        if (statdict["st_mode"] & i[0]) != 0:
+          statdict["st_mode"] = statdict["st_mode"] | i[1]
+      return statdict
 
     def getattr(self):
-      """The getattr callback is in charge of reading the metadata of a given 
-      path, this callback is always called before any operation made on the
-      filesystem.
-      
-      Returns: a dictionary with keys identical to the stat C structure of
-        stat(2)."""
-      import pdb; pdb.set_trace()
-      if self.dataset_file != None:
-        st = os.lstat(self.dataset_file)
-      else:
-        st = os.lstat(self.fullpath)
+        """The getattr callback is in charge of reading the metadata of a given path,
+           this callback is always called before any operation made on the filesystem.
         
+        We are telling FUSE that the current entry is a file
+        or a directory using the stat struct. 
+        In general, if the entry is a directory, st_mode have to be set
+        to S_IFDIR and st_nlink to 2, while if it’s a file, st_mode have 
+        to be set to S_IFREG (that stands for regular file) and st_nlink to 1.
+        Files also require that the st_size (the full file size) is specified.
+        """
+        #import pdb; pdb.set_trace()
+        if self.dataset_file != None:
+          st = os.lstat(self.dataset_file)
+        else:
+          st = os.lstat(self.fullpath)
         statdict = dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
                 'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
-        
         if self.dataset_file != None:
+          print "NEXUSHANDLE:  ", self.dataset_handle
+          print "NEXUSFILE:    ", self.dataset_file
+          print "INTERNALPATH: ", self.internalpath
           if self.internalpath == "/":
+            print "at a filepath slash..."
+            #import pdb; pdb.set_trace()
             statdict = self.makeIntoDir(statdict)
-          elif isinstance(self.dataset_handle[self.internalpath], ncpy.Dataset):
-            ob = self.dataset_handle[self.internalpath].value
-            statdict["st_size"] = ob.size * ob.itemsize
-          
-        return statdict
+            #elif isinstance(self.dataset_handle[self.internalpath],h5py.Group):
+            #	statdict = self.makeIntoDir(statdict)
+            #	statdict["st_size"] = 0
+            #elif isinstance(self.dataset_handle[self.internalpath],h5py.Dataset):
+            #	ob=self.dataset_handle[self.internalpath].value
+            #	statdict["st_size"] = ob.size * ob.itemsize
+            #elif isinstance(self.dataset_handle[self.internalpath], ncpy.Dataset): 
+          elif isinstance(self.dataset_handle, ncpy.Dataset):
+            # It is a netcdf dataset!!!
+            # So we are going to render it as a directory
+            print "getattr is dealing with a netCDF File"
+            statdict = self.makeIntoDir(statdict)
+            statdict["st_size"] = 0
+              #statdict["st_size"] = ob.size * ob.itemsize
+              #print statdict["st_size"]
+        return statdict	
 
     def getxattr(self, name):
       raise NotImplementedError()
 
+    def getncVars(self, ncfile):
+      """Returns the variables in a netcdf file"""
+      dset = ncpy.Dataset(ncfile, 'r')
+      return dset.variables
+
     def listdir(self):
+      """Overrides readdir
+      """
       if self.dataset_handle == None:
         return ['.', '..'] + [name.encode('utf-8') for name in os.listdir(self.fullpath)]
+      elif isinstance(self.dataset_handle, ncpy.Dataset):
+        # Return a list of netCDF variables
+        netCDFvars = self.getncVars(self.dataset_file)
+        return ['.', '..'] + [item.encode('utf-8') for item in netCDFvars]
       else:
-        print(self.dataset_handle, self.dataset_handle[self.internalpath])
         items = self.dataset_handle[self.internalpath].items()
-        return ['.', '..'] + [item[0].encode('utf-8') for item in items]
+        return ['.', '..'] + [item[0].encode('utf-8')  for item in items]
     
-#    def listxattr(self):
-#      if self.dataset_handle == None:
-#        return []
-#      xattrs = []
-#      for i in self.dataset_handle[self.internalpath].attrs.keys():
-#        xattrs.append("user."+i)
-#      
-#      if isinstance(self.dataset_handle[self.internalpath], ncpy.Dataset):
-#        for i in self.data_attrs.keys():
-#          xattrs.append(i)
-#      return xattrs
+    def listxattr(self):
+      raise NotImplementedError()
 
     def access(self, mode):
       path = self.fullpath
