8b972d0da515c19cc48f2e0e569a3ef40cbf5fa5
Declan Valters <declan.valters@ed.ac.uk>
2018-07-04 18:03:37 +0100

    placeholder for unittesting framework

---
+++ test/test_fusenetcdf.py
@@ -0,0 +1,12 @@
+# Tests for the fuse netcdf project
+import unittest
+
+import fusenetcdf as fnc
+
+class testFuseNetcdf(unittest.TestCase):
+    pass
+
+if __name__=='__main__':
+    unittest.main()
+
+
