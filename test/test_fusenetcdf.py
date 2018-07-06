6bd9694cd2c7b19c863e1e2c274bbe4b60021e00
Declan Valters <declan.valters@ed.ac.uk>
2018-07-06 13:18:47 +0100

    Change Octals to Py2/Py3 compatible

--- test/test_fusenetcdf.py
+++ test/test_fusenetcdf.py
@@ -1,10 +1,20 @@
 # Tests for the fuse netcdf project
 import unittest
 
-import fusenetcdf as fnc
+from fusenetcdf.NetCDFFUSE import PotentialNetCDFFile
 
 class testFuseNetcdf(unittest.TestCase):
-    pass
+
+    # Set up test file object
+    path = "trial/ECMWF_ERA-40_subset.nc"
+    ncfile = PotenitalNetCDFFile(path)
+    
+    def test_testNetCDF(self):
+      """
+          Test that the netCDF file checker returns correct
+          truthy values.
+      """
+      self.assertTrue(ncfile.testNetCDF(path))
 
 if __name__=='__main__':
     unittest.main()
