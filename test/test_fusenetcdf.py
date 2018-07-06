0b2268fbdd4bf5b23618412b63d614a16362c93d
Declan Valters <declan.valters@ed.ac.uk>
2018-07-06 14:15:10 +0100

    Fix unittest class variables scope with setUpClass classmethod

--- test/test_fusenetcdf.py
+++ test/test_fusenetcdf.py
@@ -1,20 +1,23 @@
 # Tests for the fuse netcdf project
 import unittest
 
-from fusenetcdf.NetCDFFUSE import PotentialNetCDFFile
+import fusenetcdf.fusenetcdf as fnc
+
 
 class testFuseNetcdf(unittest.TestCase):
 
-    # Set up test file object
-    path = "trial/ECMWF_ERA-40_subset.nc"
-    ncfile = PotenitalNetCDFFile(path)
-    
+    @classmethod
+    def setUpClass(self):
+       # Set up test file object
+      self.PATH = "trial/ECMWF_ERA-40_subset.nc"
+      self.NCFILE = fnc.NetCDFFUSE.PotentialNetCDFFile(self.PATH)
+
     def test_testNetCDF(self):
       """
           Test that the netCDF file checker returns correct
           truthy values.
       """
-      self.assertTrue(ncfile.testNetCDF(path))
+      self.assertTrue(self.NCFILE.testNetCDF(self.PATH))
 
 if __name__=='__main__':
     unittest.main()
