a68eaeaccfa49b4198f2fd367467c88d5f93e366
Declan Valters <declan.valters@ed.ac.uk>
2018-07-10 18:45:27 +0100

    PEP8 in main module

--- fusenetcdf/fusenetcdf.py
+++ fusenetcdf/fusenetcdf.py
@@ -33,7 +33,8 @@ class NetCDFFUSE(Operations):
         self.readwritelock = Lock()
 
     def __call__(self, operation, path, *args):
-        return super(NetCDFFUSE, self).__call__(operation, self.filerootdir + path, *args)
+        return super(NetCDFFUSE, self).__call__(operation, self.filerootdir +
+                                                path, *args)
 
     class PotentialNetCDFFile:
         """
@@ -41,11 +42,11 @@ class NetCDFFUSE(Operations):
         """
         def __init__(self, path):
             """
-            self.dsattrs = { "user.ndim" : (lambda x : x.value.ndim), 
-              "user.shape" : (lambda x : x.value.shape), 
-              "user.dtype" : (lambda x : x.value.dtype), 
-              "user.size" : (lambda x : x.value.size), 
-              "user.itemsize" : (lambda x : x.value.itemsize), 
+            self.dsattrs = { "user.ndim" : (lambda x : x.value.ndim),
+              "user.shape" : (lambda x : x.value.shape),
+              "user.dtype" : (lambda x : x.value.dtype),
+              "user.size" : (lambda x : x.value.size),
+              "user.itemsize" : (lambda x : x.value.itemsize),
               "user.dtype.itemsize" : (lambda x : x.value.dtype.itemsize),}
             """
             self.fullpath = path
@@ -62,15 +63,17 @@ class NetCDFFUSE(Operations):
                 for i in range(len(components), 0, -1):
                     test = "/".join(components[:i])
                     if self.testNetCDF(test):
-                        self.internalpath = "/".join(components[i-len(components):])
+                        self.internalpath = "/".join(
+                            components[i - len(components):])
                         print(self.internalpath)
                         break
-                        # Could handle this case better - I think it can be done better with 
+                        # Could handle this case better
+                        # I think it can be done better with
                         # the os path methods
 
         def testNetCDF(self, path):
             """
-            Check if the path is a netCDF file. 
+            Check if the path is a netCDF file.
             """
             if os.path.isfile(path):
                 try:
@@ -85,7 +88,7 @@ class NetCDFFUSE(Operations):
             return False
 
         def __del__(self):
-            if self.dataset_handle != None:
+            if self.dataset_handle is not None:
                 try:
                     self.dataset_handle.close()
                 except:
@@ -98,29 +101,33 @@ class NetCDFFUSE(Operations):
             """
             print("Making a statdict to create a folder structure!")
             statdict["st_mode"] = statdict["st_mode"] ^ 0o100000 | 0o040000
-            for i in [ [ 0o400 , 0o100 ] , [ 0o40 , 0o10 ] , [ 0o4, 0o1 ] ]:
+            for i in [[0o400, 0o100], [0o40, 0o10], [0o4, 0o1]]:
                 if (statdict["st_mode"] & i[0]) != 0:
                     statdict["st_mode"] = statdict["st_mode"] | i[1]
             return statdict
 
         def getattr(self):
-            """The getattr callback is in charge of reading the metadata of a given path,
-               this callback is always called before any operation made on the filesystem.
-            
+            """The getattr callback is in charge of reading the metadata of a
+                given path, this callback is always called before any operation
+                made on the filesystem.
+
             We are telling FUSE that the current entry is a file
-            or a directory using the stat struct. 
+            or a directory using the stat struct.
             In general, if the entry is a directory, st_mode have to be set
-            to S_IFDIR and st_nlink to 2, while if it’s a file, st_mode have 
-            to be set to S_IFREG (that stands for regular file) and st_nlink to 1.
-            Files also require that the st_size (the full file size) is specified.
+            to S_IFDIR and st_nlink to 2, while if it’s a file, st_mode have
+            to be set to S_IFREG (that stands for regular file) and st_nlink
+            to 1. Files also require that the st_size (the full file size) is
+            specified.
             """
-            if self.dataset_file != None:
+            if self.dataset_file is not None:
                 st = os.lstat(self.dataset_file)
             else:
                 st = os.lstat(self.fullpath)
-                statdict = dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
-                    'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
-            if self.dataset_file != None:
+                statdict = dict((key, getattr(st, key)) for key in
+                                ('st_atime', 'st_ctime', 'st_gid', 'st_mode',
+                                    'st_mtime', 'st_nlink',
+                                    'st_size', 'st_uid'))
+            if self.dataset_file is not None:
                 print("NETCDF_FILE:    ", self.dataset_file)
                 print("INTERNALPATH: ", self.internalpath)
                 if self.internalpath == "/":
@@ -137,7 +144,7 @@ class NetCDFFUSE(Operations):
                 elif "DATA_REPR" in self.internalpath:
                     print("WE ARE INSIDE A VARIABLE DIR: ", self.internalpath)
                     statdict["st_size"] = 0
-            return statdict	
+            return statdict
 
         def getxattr(self, name):
             raise NotImplementedError()
@@ -155,71 +162,77 @@ class NetCDFFUSE(Operations):
 
         def listdir(self):
             """Overrides readdir.
-            Called when ls or ll and any other unix command that relies 
+            Called when ls or ll and any other unix command that relies
             on this operation to work.
             """
-            if self.dataset_handle == None:
-                return ['.', '..'] + [name.encode('utf-8') for name in os.listdir(self.fullpath)]
+            if self.dataset_handle is None:
+                return (['.', '..'] +
+                        [name.encode('utf-8')
+                        for name in os.listdir(self.fullpath)])
             elif self.internalpath == "":
                 # Return a list of netCDF variables
-                return ['.', '..'] + [item.encode('utf-8') for item in self.ncVars]
+                return (['.', '..'] + [item.encode('utf-8')
+                        for item in self.ncVars])
             elif self.internalpath in self.ncVars:
                 print("GETTING ATTRIBUTES...")
                 local_attrs = self.getncAttrs(self.internalpath)
                 print("ATTRS: ", local_attrs)
                 return ['.', '..'] + local_attrs + ["DATA_REPR"]
             else:
-                return ['.', '..'] 
-        
+                return ['.', '..']
+
         def listxattr(self):
             raise NotImplementedError()
 
         def access(self, mode):
             path = self.fullpath
-            if self.dataset_file != None:
+            if self.dataset_file is not None:
                 path = self.dataset_file
             # If we can execute it, we should be able to read it too
             if mode == os.X_OK:
                 mode == os.R_OK
             if not os.access(path, mode):
                 raise FuseOSError(EACCES)
-            
+
         def read(self, size, offset, fh, lock):
-            if self.dataset_handle == None or self.internalpath == "/":
+            if self.dataset_handle is None or self.internalpath == "/":
                 with lock:
                     os.lseek(fh, offset, 0)
                     return os.read(fh, size)
-            if isinstance(self.dataset_handle[self.internalpath], ncpy.Dataset):
-                return self.dataset_handle[self.internalpath].value.tostring()[offset:offset+size]
+            if isinstance(
+                    self.dataset_handle[self.internalpath], ncpy.Dataset):
+                return self.dataset_handle[
+                        self.internalpath].value.tostring()[offset:offset+size]
 
         def open(self, flags):
-            if self.dataset_handle == None or self.internalpath == "/":
+            if self.dataset_handle is None or self.internalpath == "/":
                 return os.open(self.fullpath, flags)
             return 0
 
         def close(self, fh):
-            if self.dataset_handle == None or self.internalpath == "/":
+            if self.dataset_handle is None or self.internalpath == "/":
                 return os.close(fh)
             return 0
-      
+
         """These are the fusepy module methods that are overridden
         in this class. Any method not overridden here means that
-        the default fusepy API method will be used. 
+        the default fusepy API method will be used.
 
         (See the fusepy.Operations class)
 
         Note these are not exactly the same as the C libs for FUSE
-          
+
         """
         def acccess(self, path, mode):
             self.PotentialNetCDFFile(path).access(mode)
 
         def read(self, path, size, offset, fh):
-            return self.PotentialNetCDFFile(path).read(size, offset, fh, self.readwritelock)
+            return self.PotentialNetCDFFile(path).read(size, offset,
+                                                       fh, self.readwritelock)
 
         def getattr(self, path, fh=None):
             return self.PotentialNetCDFFile(path).getattr()
-      
+
         def getxattr(self, path, name):
             return self.PotentialNetCDFFile(path).getxattr(name)
 
@@ -263,5 +276,4 @@ if __name__ == "__main__":
         print("Usage: %s <netcdf file folder> <mountpoint>" % sys.argv[0])
         sys.exit(1)
     fuse = FUSE(NetCDFFUSE(sys.argv[1]), sys.argv[2])
-    # fuse = FUSE(NetCDFFUSE(sys.argv[1]), sys.argv[2], foreground=True) # for debugging
-
+    # fuse = FUSE(NetCDFFUSE(sys.argv[1]), sys.argv[2], foreground=True)
