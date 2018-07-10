3c09cdbc612e3669e4c6e648ae578182227a2770
Declan Valters <declan.valters@ed.ac.uk>
2018-07-10 18:46:35 +0100

    PEP8 in test module

--- test/test_fusenetcdf.py
+++ test/test_fusenetcdf.py
@@ -8,18 +8,16 @@ class testFuseNetcdf(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-       # Set up test file object
-      self.PATH = "trial/ECMWF_ERA-40_subset.nc"
-      self.NCFILE = fnc.NetCDFFUSE.PotentialNetCDFFile(self.PATH)
+        # Set up test file object
+        self.PATH = "trial/ECMWF_ERA-40_subset.nc"
+        self.NCFILE = fnc.NetCDFFUSE.PotentialNetCDFFile(self.PATH)
 
     def test_testNetCDF(self):
-      """
-          Test that the netCDF file checker returns correct
-          truthy values.
-      """
-      self.assertTrue(self.NCFILE.testNetCDF(self.PATH))
+        """
+        Test that the netCDF file checker returns correct
+        truthy values.
+        """
+        self.assertTrue(self.NCFILE.testNetCDF(self.PATH))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
-
-
